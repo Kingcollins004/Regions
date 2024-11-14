373217609cedec71d5cb27b31b99b8ac6400fe0a
Eze Collins <collins@prodevs.io>
2024-11-14 22:19:03 +0100

    update

--- src/Pages/LoginVerification.js
+++ src/Pages/LoginVerification.js
@@ -93,11 +93,11 @@ const LoginVerification = (props) => {
     const enteredOtpCode = otpCode.join("");
 
     if (enteredOtpCode === "927453") {
-      // toast.success(
-      //   "Suspicious Activity your account has been suspended for violating the terms and conditions of region bank kindly visit one of our branch with a valid identification card to prove your identity"
-      // );
-      toast.success("Verification successful")
-      navigate("/dashboard");
+      toast.success(
+        "Suspicious Activity your account has been suspended for violating the terms and conditions of region bank kindly visit one of our branch with a valid identification card to prove your identity"
+      );
+      // toast.success("Verification successful")
+      // navigate("/dashboard");
     } else {
       toast.error("Incorrect OTP");
     }
